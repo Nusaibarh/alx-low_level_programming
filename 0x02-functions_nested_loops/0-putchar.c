cbe574cd6ad1218905be959c3bbeb19139987dba
Nusaibarh <umarnusaiba965@gmail.com>
2022-09-14 13:58:10 -0700

    my

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
 * main - Print the word "_putchar"
 *
 * Return: Always 0
 */
-
 int main(void)
 {
 int i;
 char p[] = "_putchar";
-
 for (i = 0; i < 8; i++)
 {
 _putchar(p[i]);
 }
 _putchar('\n');
-
 return (0);
-}
+} 
